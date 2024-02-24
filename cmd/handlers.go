9f2863e3653f6f918628d09b70110a94836c3ccf
FIFSAK <anuar200572@gmail.com>
2024-02-24 18:00:59 +0600

    write api and handlers, handlers not work

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -11,3 +11,53 @@ func HealthCheck(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 }
+func Register(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "Register")
+	if err != nil {
+		return
+	}
+}
+func Login(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "Login")
+	if err != nil {
+		return
+	}
+}
+func UpdateUser(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "UpdateUser")
+	if err != nil {
+		return
+	}
+
+}
+func GetAllUsers(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "GetAllUsers")
+	if err != nil {
+		return
+	}
+
+}
+func SendMessage(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "SendMessage")
+	if err != nil {
+		return
+	}
+}
+func UpdateMessage(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "UpdateMessage")
+	if err != nil {
+		return
+	}
+}
+func DeleteMessage(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "DeleteMessage")
+	if err != nil {
+		return
+	}
+}
+func Notifications(writer http.ResponseWriter, request *http.Request) {
+	_, err := fmt.Fprintf(writer, "Notifications")
+	if err != nil {
+		return
+	}
+}
--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,15 @@ import (
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/health-check", HealthCheck).Methods("GET") // Теперь HealthCheck доступен без указания пакета
+	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
+	router.HandleFunc("/register", Register).Methods("POST")
+	router.HandleFunc("/login", Login).Methods("GET")
+	router.HandleFunc("/login", UpdateUser).Methods("UPDATE")
+	router.HandleFunc("/users", GetAllUsers).Methods("GET")
+	router.HandleFunc("/message/{id}", SendMessage).Methods("POST")
+	router.HandleFunc("/message/{id}", UpdateMessage).Methods("UPDATE")
+	router.HandleFunc("/message/{id}", DeleteMessage).Methods("DELETE")
+	router.HandleFunc("/notifications", Notifications).Methods("GET")
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		return
