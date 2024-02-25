e99d32f96df86e33c3a185f8fae6c581f8760309
FIFSAK <anuar200572@gmail.com>
2024-02-25 23:31:28 +0600

    add login, register, hash, User struct

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
+
+var envFile, _ = godotenv.Read(".env")
+
+type User struct {
+	id       int
+	username string
+	password string
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
 
 func HealthCheck(writer http.ResponseWriter, request *http.Request) {
 	_, err := fmt.Fprintf(writer, "OK")
@@ -12,15 +34,66 @@ func HealthCheck(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 func Register(writer http.ResponseWriter, request *http.Request) {
-	_, err := fmt.Fprintf(writer, "Register")
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", envFile["host"], envFile["port"], envFile["user"], envFile["password"], envFile["dbname"], envFile["sslmode"])
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
 		return
 	}
+	defer db.Close()
+
+	login := request.FormValue("login")
+	password := request.FormValue("password")
+
+	existingUser := User{}
+	err = db.QueryRow("SELECT username FROM users WHERE username = $1", login).Scan(&existingUser.username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Username doesn't exist, proceed with registration
+			hashPass, err := HashPassword(password)
+			if err != nil {
+				http.Error(writer, "Failed to hash password", http.StatusInternalServerError)
+				return
+			}
+			_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", login, hashPass)
+			if err != nil {
+				http.Error(writer, "Failed to insert user into database", http.StatusInternalServerError)
+				return
+			}
+			_, err = fmt.Fprintf(writer, "User registered successfully")
+			return
+		}
+		// Other error occurred during query
+		http.Error(writer, "Error checking existing user", http.StatusInternalServerError)
+		return
+	}
+
+	// Username already exists
+	http.Error(writer, "User already exists", http.StatusBadRequest)
 }
 func Login(writer http.ResponseWriter, request *http.Request) {
-	_, err := fmt.Fprintf(writer, "Login")
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", envFile["host"], envFile["port"], envFile["user"], envFile["password"], envFile["dbname"], envFile["sslmode"])
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
 		return
+	}
+	defer db.Close()
+
+	login := request.FormValue("login")
+	password := request.FormValue("password")
+
+	rows := db.QueryRow("SELECT * FROM users WHERE username = $1", login)
+	user := User{}
+	err = rows.Scan(&user.id, &user.username, &user.password)
+	if user.username == "" {
+		fmt.Fprintf(writer, "User not found")
+		return
+	}
+	if CheckPasswordHash(password, user.password) {
+		_, err = fmt.Fprintf(writer, "Logged in")
+	} else {
+		_, err = fmt.Fprintf(writer, "Wrong password")
 	}
 }
 func UpdateUser(writer http.ResponseWriter, request *http.Request) {
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module messanger
 go 1.21
 
 require github.com/gorilla/mux v1.8.1
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+)
