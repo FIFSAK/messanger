5181039aedfd35780e162db2f584cd09a116cbe7
FIFSAK <anuar200572@gmail.com>
2024-03-20 20:53:45 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ import (
 func main() {
 	// Load environment variables
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Warning: .env file not found. Falling back to environment variables.")
 	}
 
 	// Initialize the database connection
